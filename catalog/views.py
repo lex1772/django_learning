0506d8cc5142e01086bc6221ad4f35ccccadc15c
lexa1 <lexa1772@gmail.com>
2023-05-18 15:49:18 +0300

    ДЗ Сделано

---
+++ catalog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request, 'catalog/home.html')
+
+def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        print(f'You have new message from {name}({email}): {message}')
+    return render(request, 'catalog/contact.html')
